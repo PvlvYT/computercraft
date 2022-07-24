d1e67f08d68ed3be7e796878221a59885a2fb8ad
PvlvYT <71190902+PvlvYT@users.noreply.github.com>
2022-07-24 15:29:05 +0300

    Update install.lua

--- install.lua
+++ install.lua
@@ -2,6 +2,6 @@ if fs.exists("downloader.lua") then
     fs.delete("downloader.lua")
 end
 
-fs.open("downloader.lua", "w")
-fs.write(http.get("https://raw.githubusercontent.com/PvlvYT/computercraft/main/downloader.lua#", {["Cache-Control"] = "no-cache"}).readAll())
-fs.close()
+local f = fs.open("downloader.lua", "w")
+f.write(http.get("https://raw.githubusercontent.com/PvlvYT/computercraft/main/downloader.lua#", {["Cache-Control"] = "no-cache"}).readAll())
+f.close()
